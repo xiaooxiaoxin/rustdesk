642b96c4bd0c34d9e1a20e3e051d3294032fa61a
xiaoxiaoxin <2083864628@qq.com>
2025-07-03 08:25:33 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://222.173.140.166:21114".to_owned()
 }
 
 #[inline]
